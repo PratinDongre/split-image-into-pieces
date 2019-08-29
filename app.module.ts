01019fe32eaa422f6bb89a07ce2d4d7214c1c856
sumedha-prithyani <sumedhaprithyani24@gmail.com>
2019-08-29 16:27:36 +0530

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,16 @@
+
+import { AppComponent } from './app.component';
+import { PuzzleComponent } from './puzzle/puzzle.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PuzzleComponent
+    
+  ],
+  imports: [
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
